91337651954b3298936b2dd1aa1bd255d4afba4e
Nicholas Belev <80130825+NickBelev@users.noreply.github.com>
2024-05-06 10:08:43 -0400

    Update HouseRobber2.java

--- HouseRobber2.java
+++ HouseRobber2.java
@@ -1,4 +1,5 @@
 class Solution {
+    // The solution to House Robber 1
     public int rob1(int[] nums) {
         int L = 0;
         int R = 0;
@@ -11,16 +12,23 @@ class Solution {
         }
         return L;
     }
-    
+
     public int rob(int[] nums) {
+        // Edge cases that wouldn't work with a slices of our array of houses
         if (nums.length == 1) return nums[0];
         if (nums.length == 0) return 0;
+
+        // Since the houses are in a circular array, if we choose to rob the first one
+        // we must exclude the last house
+        // And vice-verse, so we make two arrays to store both of these sub-arrays
         int[] nums1 = new int[nums.length - 1];
         int[] nums2 = new int[nums.length - 1];
         for (int i = 0; i < nums.length - 1; i++) {
             nums1[i] = nums[i];
             nums2[i] = nums[i+1];
         }
+        // Then we simply find which of the two disjoint scenarios yields a better
+        // outcome when robbed, and respects the circular restriction.
         return Math.max(rob1(nums1), rob1(nums2));
     }
 }
