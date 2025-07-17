bcd1a6feb84b1ede03ab007ce5b4675be677d802
Nicholas Belev <80130825+NickBelev@users.noreply.github.com>
2025-07-17 13:08:33 -0700

    Update ZigZag_Conversion.py

--- ZigZag_Conversion.py
+++ ZigZag_Conversion.py
@@ -1,35 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
-            return s # Edge case
+        if numRows == 1 or numRows >= len(s):
+            return s
 
-        lines = []
-        for i in range(numRows):
-            lines.append([]) # Creating a 2D array with numRows rows
+        canvas = ['' for _ in range(numRows)]
+        y, direction = 0, 1
 
+        for ch in s:
+            canvas[y] += ch
+            if y == 0:
+                direction = 1
+            elif y == numRows - 1:
+                direction = -1
+            y += direction
 
-        curr_row = 0
-        down = True # True for 0->n ++, False for n->0 --
+        return ''.join(canvas)
 
-        for ch in s: # current character
-            lines[curr_row].append(ch)
-
-            if curr_row == numRows - 1 and down:
-                down = False # We reached the bottom row, start climbing back up
-            if curr_row == 0 and not down:
-                down = True # Reached the 0th row, start going back down
-
-            if down:
-                curr_row += 1 # We've still got room to count up
-            else:
-                curr_row -= 1
-
-        result = ""
-
-        for line in lines:
-            for ch in line:
-                result += ch
-        
-        return result
-
-            
